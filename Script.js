5c309424af4e0541dc4e7f209775bc82d315f9ee
Imran2472 <162036965+Imran2472@users.noreply.github.com>
2024-03-27 02:06:39 +0500

    index.html

---
+++ Script.js
@@ -0,0 +1,47 @@
+let [meliseconds, seconds, minutes, hours] = [0,0,0,0];
+let Init = null;
+let TImeDisplayTag = document.querySelector(".timer_display");
+let StartBtn = document.querySelector(".start");
+let pauseBtn = document.querySelector(".pause");
+let resetBtn = document.querySelector(".reset");
+
+StartBtn.addEventListener("click", displayTime);
+pauseBtn.addEventListener("click", () =>{
+    clearInterval(Init)
+});
+
+resetBtn.addEventListener("click",  () =>{
+    clearInterval(Init);
+  TImeDisplayTag.innerHTML = "00 : 00 : 00 : 000"
+});
+
+function displayTime() {
+
+        Init = setInterval(displayTimeFunction, 10 );
+    
+}
+
+function displayTimeFunction () {
+    meliseconds += 10;
+    if(meliseconds == 1000){
+        meliseconds = 0;
+        seconds++;
+        if(seconds == 60){
+            seconds = 0;
+            minutes++
+            if(minutes == 60){
+                minutes = 0
+                hours++
+            }
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours
+    let m = minutes < 10 ? "0" + minutes : minutes
+    let s = seconds < 10 ? "0" + seconds : seconds
+    let ms = meliseconds < 10 ? "00" + meliseconds : meliseconds < 100 ? "0" +meliseconds : meliseconds;
+
+    TImeDisplayTag.innerHTML = `${h} : ${m} : ${s} : ${ms}`
+
+}
+
